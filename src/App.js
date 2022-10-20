099f9c282643ca7777c64c0064a9d2052d396d14
calozrahael <calozraphael@gmail.com>
2022-10-20 19:51:21 -0400

    Fix xkcd API calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
+  const [appData, setAppData] = useState()
+
+  useEffect(() => {
+
+    fetch("/api/xkcd/1").then(
+      response => {
+        return response.json()
+      }
+    ).then(
+      data => {
+        setAppData(data)
+      }
+    )
+
+    // fetch("/xkcd/info.0.json").then(response => 
+    //   response.json().then(data => ({
+    //       data: data,
+    //       status: response.status
+    //   })
+    // ).then(res => {
+    //     setAppData(res.data)
+    //     console.log(res.data.img)
+    //     console.log(res.status, res.data.title)
+    // }));
+    
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={appData ? appData.img: "" } alt="comic strip"></img>
+      <h1>hi</h1>
     </div>
   );
 }
