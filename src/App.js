6db3a8f838405d5c4414f757b3784df53c9e3ce1
calozrahael <calozraphael@gmail.com>
2022-10-20 23:34:38 -0400

    Add navigation to specific comic strip through url param

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import Comic from './components/comic';
 import {useState, useEffect} from "react"
 import "./App.css"
 
-function App() {
+export default function App() {
 
+  // Use end of URL to navigate to a specific comic strip
+  let defaultComicNumber
+  console.log("comicNumber: ", comicNumber)
+  if (typeof comicNumber === "undefined"){
+    console.log("document.URL: ", document.URL)
+    const comicIdStart = document.URL.lastIndexOf('/')
+
+    console.log("comicIdStart: ", comicIdStart)
+    const urlEnd = document.URL.slice(comicIdStart).replace('/','')
+
+    console.log("urlEnd: ", urlEnd)
+    
+    if (!isNaN(+urlEnd)) { 
+      defaultComicNumber = +urlEnd
+    } else {
+      defaultComicNumber = 1  // TODO: set to max comic number
+    }
+    console.log('defaultComicNumber: ', defaultComicNumber)
+  }
+
+  // comicNumber is the only state that we need to change to change the comic strip.
+  // The comicData state is automatically changed when change comicNumber, through useEffect calls.
   const [comicData, setComicData] = useState()
-  const [comicNumber, setComicNumber] = useState(1)
+  const [comicNumber, setComicNumber] = useState(defaultComicNumber)
 
     useEffect(() => {
         fetch(`/api/xkcd/${comicNumber}`).then(
@@ -28,11 +50,28 @@ function App() {
     setComicNumber((prevComicNumber) => prevComicNumber+1)
   }
 
+  function randomizeComicNumber() {
+    fetch("/api/xkcd/").then(
+        response => {
+          return response.json()
+        }
+    ).then(
+        data => {
+          setComicData(data)
+        }
+    )
+  }
+
+  function getMaxComicId() {
+    
+  }
+
 
   return (
     <div className="App">
       <h1>XKCD Comic Strips</h1>
       <button onClick={decrComicNumber}>Previous</button>
+      <button onClick={randomizeComicNumber}>Random</button>
       <button onClick={incrComicNumber}>Next</button>
       {comicData && 
         <Comic 
@@ -46,5 +85,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
